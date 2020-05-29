77782473824c54873dd249a7e67c065e715375f4
Lucas Marques <lucasmarques73@hotmail.com>
2020-05-29 20:09:34 -0300

    Simple api

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (_, res) => res.send("OK"));
+
+app.listen(process.env.PORT || port, () =>
+  console.log(`Server running in ${port}`)
+);
