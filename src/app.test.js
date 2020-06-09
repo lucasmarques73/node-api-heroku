a012f4a872c9dad5142e120441a5eceac148732a
Lucas Marques <lucasmarques73@hotmail.com>
2020-06-09 20:08:38 -0300

    Test if code climate score down

--- src/app.test.js
+++ src/app.test.js
@@ -12,34 +12,34 @@ describe("GET / ", () => {
   });
 });
 
-describe("GET /health ", () => {
-  test("It should respond with an Healthy", async () => {
-    const response = await request(app).get("/health");
-    expect(response.text).toEqual("Healthy");
-    expect(response.statusCode).toBe(200);
-  });
-});
+// describe("GET /health ", () => {
+//   test("It should respond with an Healthy", async () => {
+//     const response = await request(app).get("/health");
+//     expect(response.text).toEqual("Healthy");
+//     expect(response.statusCode).toBe(200);
+//   });
+// });
 
-describe("GET /users ", () => {
-  test("It should respond with an array of users", async () => {
-    const response = await request(app).get("/users");
-    expect(response.body).toEqual(users);
-    expect(response.statusCode).toBe(200);
-  });
-});
+// describe("GET /users ", () => {
+//   test("It should respond with an array of users", async () => {
+//     const response = await request(app).get("/users");
+//     expect(response.body).toEqual(users);
+//     expect(response.statusCode).toBe(200);
+//   });
+// });
 
-describe("GET /users/:id ", () => {
-  const expectedUser = { id: 2, name: "Mateus" };
+// describe("GET /users/:id ", () => {
+//   const expectedUser = { id: 2, name: "Mateus" };
 
-  test(`It should respond with an user id ${expectedUser.id}`, async () => {
-    const response = await request(app).get(`/users/${expectedUser.id}`);
-    expect(response.body).toEqual(expectedUser);
-    expect(response.statusCode).toBe(200);
-  });
+//   test(`It should respond with an user id ${expectedUser.id}`, async () => {
+//     const response = await request(app).get(`/users/${expectedUser.id}`);
+//     expect(response.body).toEqual(expectedUser);
+//     expect(response.statusCode).toBe(200);
+//   });
 
-  test(`It should respond with not found user status code`, async () => {
-    const unexpectedId = 99;
-    const response = await request(app).get(`/users/${unexpectedId}`);
-    expect(response.statusCode).toBe(404);
-  });
-});
+//   test(`It should respond with not found user status code`, async () => {
+//     const unexpectedId = 99;
+//     const response = await request(app).get(`/users/${unexpectedId}`);
+//     expect(response.statusCode).toBe(404);
+//   });
+// });
